eca3ba517e8d83b1180478170c408386d2a20e6a
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2014-02-17 06:23:26 +0100

    Automatic checkout externals

--- osummarizer/ext/checkout-externals.sh
+++ osummarizer/ext/checkout-externals.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-svn checkout https://svn.model.in.tum.de/svn/vs/code/trunk/utils
-svn checkout https://svn.model.in.tum.de/svn/vs/code/trunk/clplib3
+svn checkout https://svn.model.in.tum.de/svn/vs/code/trunk/utils $(dirname $0)/utils
+#svn checkout https://svn.model.in.tum.de/svn/vs/code/trunk/clplib3 $(dirname $0)/clplib3
