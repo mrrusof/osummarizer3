5a890cc31d48a866d66fcbce934b1a376aa360ab
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2014-02-18 18:22:47 +0100

    Tests

---
+++ osummarizer/run-tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+NO_COLOR="\033[0m"
+
+if [ ! -x osummarizer ]; then
+    make osummarizer
+fi
+
+for f in `ls tests/*/*.of`; do
+    echo -n "$f ... "
+    (   ./osummarizer $f ${f%.of}.qarmc >/dev/null && \
+        echo -e ${GREEN}PASSED$NO_COLOR
+    ) || echo -e ${RED}FAILED$NO_COLOR
+done
